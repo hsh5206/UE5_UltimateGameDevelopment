f1e52a56b6138bc732e63b67bf34bce02fc1efe9
hsh5206 <hyunhsh5206@gmail.com>
2023-01-27 07:38:22 +0900

    Section8_CharacterClass

--- Source/Slash/Public/Pawn/Bird.h
+++ Source/Slash/Public/Pawn/Bird.h
@@ -18,10 +18,7 @@ class SLASH_API ABird : public APawn
 
 public:
 	ABird();
-
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
